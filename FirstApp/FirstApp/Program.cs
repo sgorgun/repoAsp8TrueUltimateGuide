c50d6ed71dd5db34405efb67840317ba45138505
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-28 16:03:31 +0300

    01. HttpContext. Header

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -8,15 +8,15 @@ app.Run(async (HttpContext context) =>
     context.Response.Headers["MyKey"] = "my value";
     context.Response.Headers["Server"] = "My Server";
     context.Response.ContentType = "text/html";
-    if (context.Request.Method == "GET")
+    if (context.Request.Headers.ContainsKey("User-Agent"))
     {
-        if (context.Request.Query.ContainsKey("id"))
-        {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"<p>Id: {id}</p>");
-            string name = context.Request.Query["name"];
-            await context.Response.WriteAsync($"<p>Name: {name}</p>");
-        }
+        string id = context.Request.Query["id"];
+        await context.Response.WriteAsync($"<p>Id: {id}</p>");
+        string name = context.Request.Query["name"];
+        await context.Response.WriteAsync($"<p>Name: {name}</p>");
+        string userAgent = context.Request.Headers["User-Agent"];
+        await context.Response.WriteAsync($"<p>User-Agent: {userAgent}</p>");
+
     }
     await context.Response.WriteAsync($"<p>{path}</p>");
     await context.Response.WriteAsync($"<p>{method}</p>");
