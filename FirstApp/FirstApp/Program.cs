0d12abd461a8d63bd2f2d5a310983551f863a6f3
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-28 16:15:58 +0300

    02. HttpContext. Postman GET Check

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -8,14 +8,14 @@ app.Run(async (HttpContext context) =>
     context.Response.Headers["MyKey"] = "my value";
     context.Response.Headers["Server"] = "My Server";
     context.Response.ContentType = "text/html";
-    if (context.Request.Headers.ContainsKey("User-Agent"))
+    if (context.Request.Headers.ContainsKey("AuthorizationKey"))
     {
         string id = context.Request.Query["id"];
         await context.Response.WriteAsync($"<p>Id: {id}</p>");
         string name = context.Request.Query["name"];
         await context.Response.WriteAsync($"<p>Name: {name}</p>");
-        string userAgent = context.Request.Headers["User-Agent"];
-        await context.Response.WriteAsync($"<p>User-Agent: {userAgent}</p>");
+        string auth = context.Request.Headers["AuthorizationKey"];
+        await context.Response.WriteAsync($"<p>Authorization Key: {auth}</p>");
 
     }
     await context.Response.WriteAsync($"<p>{path}</p>");
