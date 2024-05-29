a3f8efc7cde4243b0d0913a72fb07801829a3d46
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-29 16:57:33 +0300

    1. app.Run

---
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello");
+
+});
+
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello again");
+});
+
+app.Run();
