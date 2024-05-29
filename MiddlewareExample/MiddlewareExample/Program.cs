f60f5744b59ceaf6326bc02496c1627d2f1b524f
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-29 17:16:59 +0300

    middleware chain

--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -1,13 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello");
 
+//middleware1
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("Hello1");
+    await next(context);
 });
 
+//middleware2
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("\nHello2");
+    await next(context);
+});
+
+//middleware3
 app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello again");
+    await context.Response.WriteAsync("\nHello3");
 });
 
 app.Run();
