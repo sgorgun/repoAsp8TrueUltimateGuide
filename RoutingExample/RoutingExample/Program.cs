40dcf35b7cb82acb7d701a1eb19078ede65d63ee
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-30 21:16:39 +0300

    Routing. Default walue.

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -10,15 +10,21 @@ app.UseEndpoints(endpoints =>
     // add your endpoints here
     endpoints.Map("files/{filename}.{extension}", async context =>
     {
-        string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
-        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+        string? fileName = Convert.ToString(context.Request.RouteValues["filename=sergei"]);
+        string? extension = Convert.ToString(context.Request.RouteValues["extension=txt"]);
         await context.Response.WriteAsync($"In files: {fileName}.{extension}");
     });
 
     endpoints.Map("employee/profile/{employeeName}", async context =>
     {
-        string? employeeName = Convert.ToString(context.Request.RouteValues["employeeName"]);
+        string? employeeName = Convert.ToString(context.Request.RouteValues["employeeName=new"]);
         await context.Response.WriteAsync($"In files: {employeeName}");
+    });
+
+    endpoints.Map("products/details/{id=1}", async context =>
+    {
+        int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsync($"ID: {id}");
     });
 });
 
