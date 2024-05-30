04d811d16bdc58c5249bc6d3fd754d4021e6e321
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-30 17:01:30 +0300

    Routing. Endpoints with data

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -5,9 +5,23 @@ var app = builder.Build();
 app.UseRouting();
 
 // creating endpoints
-app.UseEndpoints(endploints => {
+app.UseEndpoints(endploints =>
+{
     // add your endpoints here
+    endploints.Map("map1", async (context) =>
+    {
+        await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+    });
 
+    endploints.Map("map2", async (context) =>
+    {
+        await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+    });
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
 });
 
 app.Run();
