303e3ad06bb5eecb34019b804f1ccb29da304eae
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-30 21:52:19 +0300

    Routing. Optional parameter.

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -8,23 +8,30 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     // add your endpoints here
-    endpoints.Map("files/{filename=document}.{extension=txt}", async context =>
+    endpoints.Map("files/{filename}.{extension}", async context =>
     {
         string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
         string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
         await context.Response.WriteAsync($"In files: {fileName}.{extension}");
     });
 
-    endpoints.Map("employee/profile/{employeeName=new}", async context =>
+    endpoints.Map("employee/profile/{employeeName?}", async context =>
     {
         string? employeeName = Convert.ToString(context.Request.RouteValues["employeeName"]);
         await context.Response.WriteAsync($"In files: {employeeName}");
     });
 
-    endpoints.Map("products/details/{id=1}", async context =>
+    endpoints.Map("products/details/{id?}", async context =>
     {
-        int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
-        await context.Response.WriteAsync($"ID: {id}");
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            await context.Response.WriteAsync($"ID: {id}");
+        }
+        else
+        {
+            await context.Response.WriteAsync("No ID found");
+        }
     });
 });
 
