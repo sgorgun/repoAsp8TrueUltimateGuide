e0971c1b65912d6218d97918fc5995662370fbc2
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-30 14:39:52 +0300

    MiddlewareAssessment - Login & Password

---
+++ MiddlewareAssessment/MiddlewareAssessment/Program.cs
@@ -0,0 +1,12 @@
+using MiddlewareAssessment.CustomMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseLoginMiddleware();
+
+app.Run(async context => {
+    await context.Response.WriteAsync("No response");
+});
+
+app.Run();
