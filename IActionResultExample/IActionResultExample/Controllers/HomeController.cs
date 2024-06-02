3f5b0b3372954c7cdfc7076c1f36e3079ca352e3
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-06-02 22:40:14 +0300

    Controllers. Status Code Results.

--- IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -10,29 +10,26 @@ namespace IActionResultExample.Controllers
             // BookID should be supplied in the query string
             if (!Request.Query.ContainsKey("bookId"))
             {
-                Response.StatusCode = 400;
-                return Content("BookID is not supplied");
+                return BadRequest("BookID is not supplied");
             }
 
             // BookID should not be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookId"])))
             {
-                Response.StatusCode = 400;
-                return Content("BookID should not be empty");
+                return BadRequest("BookID should not be empty");
             }
 
             // BookID should be between 1 and 1000
             int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookId"]);
             if (bookId < 1 || bookId > 1000)
             {
-                return Content("BookID should be between 1 and 1000");
+                return NotFound("BookID should be between 1 and 1000");
             }
 
             //is logged in should be true
             if (!Convert.ToBoolean(Request.Query["isLoggedIn"]))
             {
-                Response.StatusCode = 401;
-                return Content("User is not logged in");
+                return Unauthorized("User is not logged in");
             }
             
             return File("/sample.pdf", "application/pdf");
