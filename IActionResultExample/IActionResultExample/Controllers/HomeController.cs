ac891c862b219249c47bb18f4f9b377f3ba4a1e1
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-06-06 01:11:51 +0300

    Model Binding. Query String vs Route Data.

--- IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -4,55 +4,40 @@ namespace IActionResultExample.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("bookStore")]
-        public IActionResult Index()
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        //Url: /bookstore?bookid=10&isloggedin=true
+        public IActionResult Index(int? bookid, bool? isloggedin)
         {
-            // BookID should be supplied in the query string
-            if (!Request.Query.ContainsKey("bookId"))
+            //Book id should be applied
+            if (bookid.HasValue == false)
             {
-                return BadRequest("BookID is not supplied");
+                //return new BadRequestResult();
+                return BadRequest("Book id is not supplied or empty");
             }
 
-            // BookID should not be empty
-            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookId"])))
+            //Book id can't be less than or equal to 0
+            if (bookid <= 0)
             {
-                return BadRequest("BookID should not be empty");
+                return BadRequest("Book id can't be less than or equal to 0");
             }
 
-            // BookID should be between 1 and 1000
-            int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookId"]);
-            if (bookId < 1 || bookId > 1000)
+            //Book id should be between 1 to 1000
+            if (bookid <= 0)
             {
-                return NotFound("BookID should be between 1 and 1000");
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
             }
 
-            //is logged in should be true
-            if (Convert.ToBoolean(Request.Query["isLoggedIn"]) == false)
+            //isloggedin should be true
+            if (isloggedin == false)
             {
-                //return Unauthorized("User is not logged in");
                 return StatusCode(401);
             }
 
-            // 302 Found - RedirectToActionResult
-            //return new RedirectToActionResult("Books", "Store", new { });  // 302 Found
-            //return RedirectToAction("Books", "Store", new { id = bookId });
-
-            // 301 Moved Permanently - RedirectToActionResult
-            // return new RedirectToActionResult("Books", "Store", new { }, true); // 301 Moved Permanently
-            //return RedirectToActionPermanent("Books", "Store", new { id = bookId });
-
-            // 302 Found - RedirectToRouteResult
-            //return new LocalRedirectResult($"store/books/{bookId}");
-            //return LocalRedirect($"store/books/{bookId}"); // 302 Found
-
-            // 301 Moved Permanently - RedirectToRouteResult
-            return new LocalRedirectResult($"store/books/{bookId}", true); // 301 Moved Permanently
-            //return LocalRedirectPermanent($"store/books/{bookId}"); // 301 Moved Permanently
-
-            //return Redirect($"store/books/{bookId}"); // 302 Found
-            //return RedirectPermanent($"store/books/{bookId}"); // 301 Moved Permanently
+            return Content($"Book id: {bookid}", "text/plain");
         }
     }
 }
-
-// http://localhost:5211/bookstore?bookid=5&isLoggedIn=true
--- IActionResultExample/IActionResultExample/Controllers/StoreController.cs
+++ IActionResultExample/IActionResultExample/Controllers/StoreController.cs
@@ -4,11 +4,10 @@ namespace IActionResultExample.Controllers
 {
     public class StoreController : Controller
     {
-        
         [Route("store/books/{id}")]
         public IActionResult Books()
         {
-            int id = Convert.ToInt32(Request.RouteValues["id"]);    
+            int id = Convert.ToInt32(Request.RouteValues["id"]);
             return Content($"<h1>Book Store {id}</h1>", "text/html");
         }
     }
