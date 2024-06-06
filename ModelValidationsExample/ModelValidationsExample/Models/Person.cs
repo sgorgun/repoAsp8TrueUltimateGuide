cdf41b3abd0954b116cd38a52bf0a37159ba7776
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-06-06 17:41:15 +0300

    Model binding. Model state.

--- ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -4,7 +4,7 @@ namespace ModelValidationsExample.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "Person name can't be empty or null")]
         public string? PersonName { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
